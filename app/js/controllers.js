4095b86d03d48d9d6772e18722b7fad2add38d21
Fernando Garcia <fgarcia@thinkPad.(none)>
2013-03-26 10:27:46 -0300

    selects de mes y año

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,25 +10,49 @@
 
  experienceList:[{ company:'Tecso', 
                 title:'SSr Developer',
-                fromMonth:'08',
-                fromYear:'2011',
-                toMonth:'03',
-                toYear:'2013',
+                fromMonth:8,
+                fromYear:2011,
+                toMonth:3,
+                toYear:2013,
                 description: 'Participe en el desarrollo de aplicaciones para el cliente CitBank'
               },
 
               { company:'IBM', 
                 title:'Junior Developer',
-                fromMonth:'01',
-                fromYear:'2010',
-                toMonth:'08',
-                toYear:'2011',
+                fromMonth:1,
+                fromYear:2010,
+                toMonth:8,
+                toYear:2011,
                 description: 'Participe en el desarrollo de aplicaciones para el cliente CitBank'
               }
 
               ]
 
   };
+
+ 
+  $scope.months = [
+    {name: 'Enero', id: 1},
+    {name: 'Febrero', id: 2},
+    {name: 'Marzo', id: 3},
+    {name: 'Abril', id: 4},
+    {name: 'Mayo', id: 5},
+    {name: 'Junio', id: 6},
+    {name: 'Julio', id: 7},
+    {name: 'Agosto', id: 8},
+    {name: 'Septiembre', id: 9},
+    {name: 'Octubre', id: 10},
+    {name: 'Noviembre', id: 11},
+    {name: 'Diciembre', id: 12}];
+
+  var currentYear = new Date().getFullYear();
+  var years = [];
+  for(var i=currentYear;i>1990;i--) {
+    years.push(i);
+  }
+  $scope.years = years;
+
+  
 
 
   $scope.cancel = function() {
@@ -41,7 +65,7 @@
   };
 
   $scope.addExperience = function() {
-    $scope.form.experienceList.push({type:'', value:''});
+    $scope.form.experienceList.unshift({type:'', value:''});
   };
 
   $scope.removeExperience = function(experience) {
